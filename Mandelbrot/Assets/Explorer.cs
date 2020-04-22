20687a65e49f2c80011b8ec32c3b316997c03dc4
dkim <dkim>
2020-04-22 13:37:46 -0300

    C# Script has been added and basic scaling behavior added

---
+++ Mandelbrot/Assets/Explorer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explorer : MonoBehaviour
+{
+
+    public Material mat;
+    public Vector2 pos;
+    public float scale;
+
+    // Update is called once per frame
+    void Update(){
+        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scale, scale));
+    }
+}
