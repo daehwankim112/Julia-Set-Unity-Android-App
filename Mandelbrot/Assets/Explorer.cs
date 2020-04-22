e79cb023e726074f519449e8db1ee2c71440e315
dkim <dkim>
2020-04-22 13:55:46 -0300

    Streching by image resolution has been fixed using aspect

--- Mandelbrot/Assets/Explorer.cs
+++ Mandelbrot/Assets/Explorer.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Explorer : MonoBehaviour
-{
+public class Explorer : MonoBehaviour {
 
     public Material mat;
     public Vector2 pos;
     public float scale;
 
     // Update is called once per frame
-    void Update(){
-        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scale, scale));
+    void Update() {
+        float aspect = (float)Screen.width / (float)Screen.height; //These are integer so it should be casted to float otherwise, it will be truncated.
+
+        float scaleX = scale;
+        float scaleY = scale;
+
+        if ( aspect > 1f ) {
+            scaleY /= aspect;
+        }
+        else {
+            scaleX *= aspect;
+        }
+
+        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scaleX, scaleY));
     }
 }
