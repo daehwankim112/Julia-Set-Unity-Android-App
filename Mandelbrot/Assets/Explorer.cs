5a1c9f2692043e02d456b6c60a322ff3fe844e03
dkim <dkim>
2020-04-22 14:05:06 -0300

    Basic Zoom in and out function using keypad + and - added

--- Mandelbrot/Assets/Explorer.cs
+++ Mandelbrot/Assets/Explorer.cs
@@ -8,20 +8,42 @@ public class Explorer : MonoBehaviour {
     public Vector2 pos;
     public float scale;
 
-    // Update is called once per frame
-    void Update() {
+    private void UpdateShaer()
+    {
         float aspect = (float)Screen.width / (float)Screen.height; //These are integer so it should be casted to float otherwise, it will be truncated.
 
         float scaleX = scale;
         float scaleY = scale;
 
-        if ( aspect > 1f ) {
+        if (aspect > 1f)
+        {
             scaleY /= aspect;
         }
-        else {
+        else
+        {
             scaleX *= aspect;
         }
 
         mat.SetVector("_Area", new Vector4(pos.x, pos.y, scaleX, scaleY));
+    }
+
+    private void HandleInputs()
+    {
+        if(Input.GetKey(KeyCode.KeypadPlus))
+        {
+            scale *= .99f;
+        }
+        if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            scale *= 1.01f;
+        }
+    }
+
+    // The difference between update and fixed update is that the fixed update execute fixed amount of update per seconds
+    //while normal update depends on how fast your computer is.
+    void FixedUpdate()
+    {
+        HandleInputs();
+        UpdateShaer();
     }
 }
