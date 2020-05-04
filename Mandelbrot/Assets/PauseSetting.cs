05c4b545d69a3c9ada1c0b8e6ffaab76e6c5e953
dkim <dkim>
2020-05-04 17:15:29 -0300

    Red, Green, Blue, C_Real, C_imaginary sliders backend completed

---
+++ Mandelbrot/Assets/PauseSetting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSetting : MonoBehaviour
+{
+
+    public Material mat;
+
+    public void SetRed(float red)
+    {
+        mat.SetFloat("_R", red);
+    }
+
+    public void SetGreen(float green)
+    {
+        mat.SetFloat("_G", green);
+    }
+
+    public void SetBlue(float blue)
+    {
+        mat.SetFloat("_B", blue);
+    }
+
+    public void SetC_Real(float real)
+    {
+        mat.SetFloat("_C_real", real);
+    }
+
+    public void SetC_Imaginary(float imaginary)
+    {
+        mat.SetFloat("_C_imaginary", imaginary);
+    }
+
+}
